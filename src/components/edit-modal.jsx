a3eaedd06686e5d0a2f53278eb3679c9ed6b2a28
balongbui2k <balongbui2k@gmail.com>
2024-01-02 10:01:45 +0700

    [chore]: delete unuse component

--- src/components/edit-modal.jsx
+++
@@ -1,9 +0,0 @@
-const CustomEditModal = ({ onClick }) => {
-  return (
-    <button className="bg-gray-300" onClick={onClick}>
-      Edit
-    </button>
-  );
-};
-
-export default CustomEditModal;
